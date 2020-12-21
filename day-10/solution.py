774309098fc68bb4227eef99841a10d00bba576c
Mark F. Brown <mark.brown314@gmail.com>
2020-12-20 23:32:08 -0500

    Pylint other finishing touches on AOC'19-10.1
    Signed-off-by: Mark F. Brown <mark.brown314@gmail.com>

--- day-10/solution.py
+++ day-10/solution.py
@@ -7,28 +7,36 @@ import sys
 import math
 
 def main():
+    """ solution for Day #10 """
     filename="puzzle_test.txt" if len(sys.argv) < 2 else sys.argv[1]
     file_input = open(filename).readlines()
     astro_map = set()
     astro_dict = dict()
-    for i in range(0, len(file_input)):
-        for j in range(0, len(file_input[i])):
-            if file_input[i][j] == "#":
+
+    # build a map of asteroids coordinates
+    for i, line in enumerate(file_input):
+        for j, char in enumerate(line):
+            if char == "#":
                 astro_map.add((j,i))
-    for c in astro_map:
-        astro_dict[c] = set()
-        for c2 in astro_map:
-            if c == c2:
+    for coord in astro_map:
+        astro_dict[coord] = set()
+        for coord2 in astro_map:
+            if coord == coord2:
                 continue
-            slope = math.atan2(c2[1]-c[1],c2[0]-c[0])
-            astro_dict[c].add(slope)
-    max_c = None
-    max_items = -1
-    for c in astro_dict:
-        if len(astro_dict[c]) > max_items:
-            max_c = c
-            max_items = len(astro_dict[c])
-    print("part #1", max_c, max_items)
+            # get angle between asteroid at coord and coord2
+            slope = math.atan2(coord2[1]-coord[1],coord2[0]-coord[0])
+            # only store if unique
+            # asteroids at same angle may occlude each other, so store only one angle
+            # angle count is equivalent to visible asteroid count
+            astro_dict[coord].add(slope)
+    best_coord = None
+    max_visible = -1
+    # find coord that can see most asteroids
+    for coord in astro_dict:
+        if len(astro_dict[coord]) > max_visible:
+            best_coord = coord
+            max_visible = len(astro_dict[coord])
+    print("part #1", best_coord, max_visible)
 
 if __name__ == "__main__":
     main()
